43ee6c932c5773674fec5ede89c6749b0e0d2e60
Ted Mielczarek <ted@mielczarek.org>
2018-05-02 17:02:33 -0400

    Write a lousy rfc3339 parser with nom

--- src/rayon.rs
+++ src/rayon.rs
@@ -1,8 +1,8 @@
 extern crate csv;
 extern crate flate2;
-#[macro_use]
-extern crate serde_derive;
 extern crate chrono;
+#[macro_use]
+extern crate nom;
 extern crate rayon;
 
 use std::boxed::Box;
@@ -11,19 +11,46 @@ use std::fs::File;
 use std::iter::Sum;
 use std::str::{self, FromStr};
 
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Duration, NaiveDate, NaiveTime, NaiveDateTime, Utc};
 use csv::{ByteRecord, ReaderBuilder};
 use flate2::read::GzDecoder;
 use rayon::prelude::*;
 
-#[derive(Debug, Deserialize)]
-struct Row {
-    bucket: String,
-    key: String,
-    size: usize,
-    last_modified_date: DateTime<Utc>,
-    etag: String,
-}
+named!(int32_4 <&str, i32>,
+    map_res!(take!(4), FromStr::from_str)
+);
+
+named!(uint32_2 <&str, u32>,
+    map_res!(take!(2), FromStr::from_str)
+);
+
+named!(uint32_3 <&str, u32>,
+    map_res!(take!(3), FromStr::from_str)
+);
+
+named!(rfc3339<&str, DateTime<Utc>>,
+  do_parse!(
+      year:  int32_4  >>
+          tag!("-") >>
+          month: uint32_2 >>
+          tag!("-") >>
+          day:  uint32_2 >>
+          tag!("T") >>
+          hour: uint32_2 >>
+          tag!(":") >>
+          minute: uint32_2 >>
+          tag!(":") >>
+          second: uint32_2 >>
+          tag!(".") >>
+          milli: uint32_3 >>
+          tag!("Z") >>
+          (DateTime::<Utc>::from_utc(
+              NaiveDateTime::new(NaiveDate::from_ymd(year, month, day),
+                                 NaiveTime::from_hms_milli(hour, minute, second, milli)),
+              Utc)
+          )
+  )
+);
 
 struct Stats {
     total: usize,
@@ -40,6 +67,8 @@ impl Sum for Stats {
         acc
     }
 }
+
+// 2008-09-08T22:47:31Z
 
 fn main() {
     let cutoff = Utc::now() - Duration::days(180);
@@ -67,12 +96,26 @@ fn count(path: &str, cutoff: DateTime<Utc>) -> Result<Stats, Box<std::error::Err
         total += 1;
         if let Some(bytes) = record.get(3) {
             let s = str::from_utf8(bytes)?;
-            let dt = DateTime::<Utc>::from_str(s)?;
-            if dt > cutoff {
-                recent += 1
-            }
+            rfc3339(s).map(|dt| {
+                if dt > cutoff {
+                    recent += 1
+                }
+            });
         }
     }
 
     Ok(Stats { total, recent })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        const S: &str = "2015-10-27T17:38:38.000Z";
+        let dt = DateTime::<Utc>::from_str(S).unwrap();
+        let (_, dt2) = rfc3339(S).unwrap();
+        assert_eq!(dt, dt2);
+    }
 }
